5b510bae17efd356709b2fb051d9759977bfa6c1
arunpanigrahi93 <arunpanigrahi94@gmail.com>
2024-09-07 16:50:47 +0530

    seperate env file for mongoose

--- server/server.js
+++ server/server.js
@@ -2,20 +2,23 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
+const dotenv = require("dotenv");
+
+// Load environment variables from .env file
+dotenv.config();
 
 // Connect to MongoDB
-
 mongoose
-  .connect("mongodb+srv://arun1234:arun1234@cluster0.x8irt.mongodb.net/")
+  .connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB connected"))
-  .catch((error) => console.log(error));
+  .catch((error) => console.log("MongoDB connection error:", error));
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: "http://localhost:5173/",
+    origin: "http://localhost:5173",
     methods: ["GET", "POST", "DELETE", "PUT"],
     allowedHeaders: [
       "Content-Type",
@@ -30,4 +33,4 @@ app.use(
 app.use(cookieParser());
 app.use(express.json());
 
-app.listen(PORT, () => console.log(`server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
